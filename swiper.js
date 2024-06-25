a88329ee6aeaacb26cd0aff3947c45962882fb7d
MaxYashch <maxyashch@gmail.com>
2024-06-25 22:12:57 +0300

    tried scroll-snap

--- swiper.js
+++ swiper.js
@@ -1,29 +1,47 @@
-import Swiper from 'swiper';
-import 'swiper/css';
+// import Swiper from 'swiper';
+// import 'swiper/css';
 
-import { register } from 'swiper/element/bundle';
+// import { register } from 'swiper/element/bundle';
 
-register();
+// register();
 
-const swiper = new Swiper('.swiper', {
-  // // Optional parameters
-  // direction: 'vertical',
-  // loop: true,
+// const swiper = new Swiper('.swiper', {
+//   // // Optional parameters
+//   // direction: 'vertical',
+//   // loop: true,
 
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
+//   // // If we need pagination
+//   // pagination: {
+//   //   el: '.swiper-pagination',
+//   // },
 
-  // // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
+//   // // Navigation arrows
+//   // navigation: {
+//   //   nextEl: '.swiper-button-next',
+//   //   prevEl: '.swiper-button-prev',
+//   // },
 
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-});
+//   // // And if we need scrollbar
+//   // scrollbar: {
+//   //   el: '.swiper-scrollbar',
+//   // },
+// });
 
+// const gra = function(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+// const gri = function(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// const init = function() {
+//   let items = document.querySelectorAll('.tariffs-mobile .tariff');
+//   for (let i = 0; i < items.length; i++) {
+//     items[i].style.minWidth = gra(30,60) + 'vw';
+//     items[i].style.background = randomColor({luminosity: 'light'});
+//   }
+//   cssScrollSnapPolyfill()
+// }
+
+// init();
